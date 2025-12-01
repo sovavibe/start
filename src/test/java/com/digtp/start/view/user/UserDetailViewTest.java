9f0cab40edb4786454d0376d83372cf16881a106
Aleksandr Polenkov <Aleksandr.Polenkov@eurochem.ru>
2025-12-01 18:52:15 +0300

    fix(test): fix UserDetailViewTest compilation errors
    - Use UiTestUtils.getComponent() instead of direct private field access
    - Use JmixButton type for button component
    - Use ComboBox type for timeZoneField component
    - Follows Jmix UI testing best practices from UserUiTest

--- src/test/java/com/digtp/start/view/user/UserDetailViewTest.java
+++ src/test/java/com/digtp/start/view/user/UserDetailViewTest.java
@@ -3,7 +3,9 @@ package com.digtp.start.view.user;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.digtp.start.test_support.AuthenticatedAsAdmin;
+import com.vaadin.flow.component.combobox.ComboBox;
 import io.jmix.flowui.ViewNavigators;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.testassist.FlowuiTestAssistConfiguration;
 import io.jmix.flowui.testassist.UiTest;
 import io.jmix.flowui.testassist.UiTestUtils;
@@ -25,12 +27,14 @@ class UserDetailViewTest {
     void testUserDetailViewInit() {
         viewNavigators.view(UiTestUtils.getCurrentView(), UserListView.class).navigate();
         final UserListView listView = UiTestUtils.getCurrentView();
-        UiTestUtils.getComponent(listView, "createButton").click();
+        final JmixButton createButton = UiTestUtils.getComponent(listView, "createButton");
+        createButton.click();
 
         final UserDetailView view = UiTestUtils.getCurrentView();
 
         assertThat(view).isNotNull();
-        assertThat(view.timeZoneField).isNotNull();
-        assertThat(view.timeZoneField.getListDataView().getItems().count()).isGreaterThan(0);
+        final ComboBox<String> timeZoneField = UiTestUtils.getComponent(view, "timeZoneField");
+        assertThat(timeZoneField).isNotNull();
+        assertThat(timeZoneField.getListDataView().getItems().count()).isGreaterThan(0);
     }
 }
